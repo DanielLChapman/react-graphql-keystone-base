f3a0806d7b4fafde8947b21ab78c91115e22ef17
DanielLChapman <danielc@my.ccsu.edu>
2021-04-28 17:29:56 -0400

    First commit

---
+++ backend/keystone.ts
@@ -0,0 +1,62 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/indent */
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Role } from './schemas/Role';
+import { sendPasswordResetEmail } from './lib/mail';
+import { permissionsList } from './schemas/fields';
+
+
+const databaseURL =
+    process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+    maxAge: 60 * 60 * 24 * 360, // how long should they stay signed in
+    secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+    listKey: 'User',
+    identityField: 'email',
+    secretField: 'password',
+    initFirstItem: {
+        fields: ['name', 'email', 'password'],
+        // todo: add in initial roles
+    },
+    passwordResetLink: {
+        async sendToken(args) {
+            // send the email
+            await sendPasswordResetEmail(args.token, args.identity);
+        },
+    },
+});
+
+export default withAuth(config({
+    server: {
+        cors: {
+            origin: [process.env.FRONTEND_URL],
+            credentials: true,
+        },
+    },
+    db: {
+        adapter: 'mongoose',
+        url: databaseURL,
+    },
+    lists: createSchema({
+        // schema items go in here
+        User,
+        Role,
+    }),
+    ui: {
+        // show the UI only for the people who pass this test
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+        User: `id name email role { ${permissionsList.join(' ')}}`
+    })
+
+}));
